723a82d551b42f2e8ef804df4c0650965c930f48
Keiji <BB123021@sun.ac.jp>
2025-04-03 10:46:36 +0900

    complete commit

--- static/js/main.js
+++ static/js/main.js
@@ -1,9 +1,14 @@
 document.addEventListener("DOMContentLoaded", () => {
     // Determine the base path dynamically
-    const basePath = window.location.pathname.includes("/menu/") || window.location.pathname.includes("/about/") ? "../" : "";
+    const isJapanese = window.location.pathname.includes("/japanese/");
+    const basePath = isJapanese
+        ? "../../"
+        : window.location.pathname.includes("/menu/") || window.location.pathname.includes("/about/")
+        ? "../"
+        : "";
 
     // Load navigation
-    fetch(`${basePath}nav.html`)
+    fetch(`${basePath}${isJapanese ? "nav_japanese.html" : "nav.html"}`)
         .then(response => response.text())
         .then(data => {
             document.getElementById("nav-placeholder").innerHTML = data;
